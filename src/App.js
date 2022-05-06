8279af02b2d5c07e6048dd6bbb5ccd544f03f80a
Methujan <methujan_02@hotmail.com>
2022-05-05 23:32:17 -0400

    set inputText

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
       <header className="App-header">
